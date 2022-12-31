cb78abee9a58683d04e99625cc045293aa316a38
bhanukh <brkhandelwal1997@gmail.com>
2022-12-31 12:13:02 +0530

    first

---
+++ src/operations/Users.js
@@ -0,0 +1,9 @@
+
+export const processUsersResponse = (users)=> {
+    return users.map(user => {
+        user.selected = false;
+        user.edit = false;
+        user.show = true;
+        return user;
+    })
+} 
